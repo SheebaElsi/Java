1aef2f777f8c8c0ea3377f8956f8dddc8fad81f7
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-07-20 18:47:02 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+public class sum 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		 int a = 12, b = 150, sum;
+	      sum = a + b;
+
+	      System.out.print("Sum is: "+sum);
+
+	}
+
+}
