19d0f057b43bea8216148ea022d08f30b3bf37e1
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-07-20 18:41:20 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,13 @@
+public class Elsi{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sheeba Elsi");
+		System.out.print("Welcome to Java World");
+
+	}
+
+}
