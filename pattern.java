5a53fe6d7b08a35a459351cec4afba3ebb1265b7
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-07-22 16:56:42 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,11 @@
+public class pattern1 {
+ 
+    public static void main(String[] args) 
+    {
+
+        System.out.println("   S      h       ee    b a ");
+        System.out.println("   Se            l b    aaa h");
+        System.out.println("J  aaaaaa v     aaa");
+        System.out.println(" We  lc      mes  ***** Yo oooo uuuu****");
+    }
+}
