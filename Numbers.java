9e1bdbfdde369e1c1368e1a66f777969a294ef6e
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-07-25 19:35:51 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class unique {
+    
+ public static void main(String[] args)
+ {
+		int value = 0;
+		for(int i = 1; i <= 4; i++)
+		{
+			for(int j = 1; j <= 4; j++)
+			{
+				for(int k = 1; k <= 4; k++)
+				{
+					if(k != i && k != j && i != j)
+					{
+						value++;
+						System.out.println(i + "" + j + "" + k);
+					}
+				}
+			}
+		}
+		System.out.println("Total number of the three-digit-numbers is " + value);
+	}
+}
