578e7fc8ad4f9554ff8a5fa98c82159d42cd8dde
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-09 15:11:25 +0530

    Add files via upload

---
+++ ex28.java
@@ -0,0 +1,16 @@
+public	class exe28
+{
+void run()
+{
+System.out.println("Vehicle is running");
+}
+}
+class Bike extends exe28
+{
+
+public static void main(String args[])
+{
+Bike obj = new Bike();
+obj.run();
+}
+}
