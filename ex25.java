91d0b59e1b87fd5aa0dacb71b148455abc73ed55
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-09 10:20:36 +0530

    Add files via upload

---
+++ ex25.java
@@ -0,0 +1,18 @@
+public 	class exe25
+{
+exe25 getA()
+{
+return this;
+}
+void msg()
+{System.out.println("Hello Sheeba..!! We Welcome You..!!");
+}
+}
+
+class Test1
+{
+public static void main(String args[])
+{
+new exe25().getA().msg();
+}
+}
