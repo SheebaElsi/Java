ec58c0c179caa4d337cc06a4a8a4874edb07d924
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-09 10:21:30 +0530

    Add files via upload

---
+++ ex26.java
@@ -0,0 +1,14 @@
+public	class exe26
+{
+float salary=100000;
+}
+class Programmer extends exe26
+{
+int bonus=19000;
+public static void main(String args[])
+{
+Programmer p=new Programmer();
+System.out.println("Programmer salary is:"+p.salary);
+System.out.println("Bonus of Programmer is:"+p.bonus);
+}
+}
