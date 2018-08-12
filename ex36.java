b46acd5889736aae47957bec6fcda8505e599248
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-12 15:23:46 +0530

    Add files via upload

---
+++ ex36.java
@@ -0,0 +1,18 @@
+public	final class exe36
+{
+
+}
+
+class Honda1 extends exe36
+{
+void run()
+{
+System.out.println("running safely with 100kmph");
+}
+
+public static void main(String args[])
+{
+Honda1 honda= new Honda1();
+honda.run();
+}
+}
---
+++ ex37.java
@@ -0,0 +1,15 @@
+public class exe37
+{
+final void run()
+{
+System.out.println("Running...");
+
+}
+}
+class Honda2 extends exe37
+{
+public static void main(String args[])
+{
+new Honda2().run();
+}
+}
