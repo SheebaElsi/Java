eb9af8fa8b3575a472574cf5163e7477f1117527
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-12 15:18:28 +0530

    Add files via upload

---
+++ ex32.java
@@ -0,0 +1,22 @@
+public	class exe32
+
+{
+exe32()
+{
+System.out.println("Vehicle is thus created");
+}
+}
+
+class Bike5 extends exe32
+{
+Bike5()
+{
+super();
+System.out.println("Bike is created");
+}
+public static void main(String args[])
+{
+Bike5 b=new Bike5();
+
+}
+}
