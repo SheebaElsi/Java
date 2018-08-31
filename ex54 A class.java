037793664f23b5e4ea9df1356cc6e19b7d08169d
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-31 20:31:47 +0530

    Add files via upload

---
+++ ex54 A class.java
@@ -0,0 +1,11 @@
+	package exe44;  
+	class A{  
+	  void msg(){System.out.println("Welcome Sheeba");
+	  }
+
+	public void msg1()
+	{
+		// TODO Auto-generated method stub
+		
+	}  
+	}  
---
+++ ex54 B class.java
@@ -0,0 +1,7 @@
+	package exe44;  
+	class B{  
+	  public static void main(String args[]){  
+	   A obj = new A();//Compile Time Error  
+	   obj.msg();//Compile Time Error  
+	  }  
+	}  
