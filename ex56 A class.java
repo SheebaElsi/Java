672f1e0add692f00d43abb23db0de9817aed6fb5
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-31 20:32:48 +0530

    Add files via upload

---
+++ ex56 A class.java
@@ -0,0 +1,9 @@
+	//save by A.java  
+package ex45;  
+	public class exer45
+	{  
+	protected void msg()
+	{
+		System.out.println("Information Technology");
+	}  
+	}  
---
+++ ex56 B class.java
@@ -0,0 +1,13 @@
+	package ex45;  
+	  
+	class B extends exer45{  
+	  public static void main(String args[]){  
+	   B obj = new B();  
+	   obj.msg();  
+	  }
+
+	private void msg1() {
+		// TODO Auto-generated method stub
+		
+	}  
+	}  
