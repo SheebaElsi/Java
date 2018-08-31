7e07b352b3f0b531f126bd33827d03386760995a
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-31 20:33:36 +0530

    Add files via upload

---
+++ ex58 A class.java
@@ -0,0 +1,9 @@
+	  
+	package exe46;  
+	public class exe46A
+	{  
+	public void msg()
+	{
+		System.out.println("GOD Loves You..!!");
+	}  
+	}  
---
+++ ex58 B class.java
@@ -0,0 +1,11 @@
+	package exe46;  
+ 
+	  
+	class exe46B
+	{  
+  public static void main(String args[])
+  {  
+	   exe46A obj = new exe46A();  
+	   obj.msg();  
+	  }  
+  }  
