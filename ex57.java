40a732221db52ef732bb0382ae16b8fe54886f1b
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-31 20:33:17 +0530

    Add files via upload

---
+++ ex57.java
@@ -0,0 +1,18 @@
+package exe50;
+
+	public class ex50
+	{  
+	public static void main(String args[]){  
+	  
+	int a[]=new int[5];//declaration and instantiation  
+	a[0]=100;//initialization  
+	a[1]=200;  
+	a[2]=709;  
+	a[3]=400;  
+a[4]=50;  
+	  
+	//printing array  
+for(int i=0;i<a.length;i++)//length is the property of array  
+	System.out.println(a[i]);  
+	  
+	}}  
