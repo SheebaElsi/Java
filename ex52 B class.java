250d67a14380e6a188df6ccf741351706e94e3e6
SheebaElsi <40590295+SheebaElsi@users.noreply.github.com>
2018-08-31 20:31:11 +0530

    Add files via upload

---
+++ ex52 B class.java
@@ -0,0 +1,9 @@
+	package exe47;  
+	class ex47B
+	{  
+	public static void main(String[] args){  
+	ex47a s=new ex47a();  
+	s.setName("GOD MY SAVIOR");  
+	System.out.println(((ex47a) s).getName());  
+	}  
+	}  
